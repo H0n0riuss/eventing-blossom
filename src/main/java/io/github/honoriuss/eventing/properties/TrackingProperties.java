938bcf6a8cebc736bca10f8833903199cbaff680
H0n0riuss <joscha.reichenberg@web.de>
2024-08-08 12:30:31 +0200

    :sparkles: adding property reading for intellij

--- src/main/java/io/github/honoriuss/eventing/properties/TrackingProperties.java
+++ src/main/java/io/github/honoriuss/eventing/properties/TrackingProperties.java
@@ -1,9 +1,11 @@
 package io.github.honoriuss.eventing.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableConfigurationProperties
 @ConfigurationProperties(prefix = "blossom")
 public class TrackingProperties {
     private boolean eventing;
